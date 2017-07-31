d56816694655b98d452d232b7ee3eda168ef97bb
AlbertOsinaga <luis.osinaga@gmail.com>
2017-07-31 11:03:30 -0400

    Inicialización de Proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MacT4SuccText
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
